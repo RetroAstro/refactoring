493386447d5a0bb7c78bbfb5756510ddf74fd014
RetroAstro <452461114@qq.com>
2019-09-14 12:46:18 +0800

    update

--- tests/building-tests/index.spec.ts
+++ tests/building-tests/index.spec.ts
@@ -2,8 +2,23 @@ import { expect } from 'chai'
 import { Province, sampleProvinceData } from '../../src/building-tests'
 
 describe('province', () => {
+  let asia: Province
+
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData())
+  })
+
   it('shortfall', () => {
-    const asia = new Province(sampleProvinceData())
     expect(asia.shortfall).equal(5)
+  })
+
+  it('profit', () => {
+    expect(asia.profit).equal(230)
+  })
+
+  it('change production', () => {
+    asia.producers[0].production = 20
+    expect(asia.shortfall).equal(-6)
+    expect(asia.profit).equal(292)
   })
 })
